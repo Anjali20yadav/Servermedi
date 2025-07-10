c852a1bd7c6ceb4179419d5088997c1ec0f0873c
anjali yadav <anjali.kmri2541@gmail.com>
2025-07-10 11:08:59 +0530

    Add deployed frontend URL to CORS origin

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
- // ✅ Load .env variables before anything else
 require('dotenv').config();
 
 const express = require('express');
-
 const cors = require('cors');
 const connectDB = require('./config/db');
 require('./reminderScheduler');
 
-console.log("🔍 MONGO_URI:", process.env.MONGO_URI); // 🔍 Check if it's loading
+const authMiddleware = require('./middleware/authMiddleware');
+
+console.log("🔍 MONGO_URI:", process.env.MONGO_URI);
 
 const app = express();
 
-// Connect to MongoDB
 connectDB();
 
-
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: [
+    'http://localhost:5173',
+    'https://healsync-frontend-ec4u.vercel.app'
+  ],
   credentials: true
 }));
 
 app.use(express.json());
 
-// Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/medicine', require('./routes/medicine'));
+app.get('/', (req, res) => {
+  res.send('🚀 Backend is running!');
+});
 
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/medicine', authMiddleware, require('./routes/medicine'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
